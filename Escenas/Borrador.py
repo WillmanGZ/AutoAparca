6ed4601be633f39520eedf83521d2cb197b45a5d
Willman <willmang@uninorte.edu.co>
2024-04-26 12:01:43 -0500

    Update Borrador.py

--- Escenas/Borrador.py
+++ Escenas/Borrador.py
@@ -1,3 +1,4 @@
+import re 
 import tkinter as tk
 import customtkinter as ctk
 
@@ -35,8 +36,9 @@ class Parqueadero:
         boton_piso1 = ctk.CTkButton(master=self.principal,
                                     text= "Piso 1",
                                     command= self.botonPiso1,
-                                    height= 20,
+                                    height= 25,
                                     width= 200,
+                                    corner_radius= 15
                                     )
         boton_piso1.grid(row = 0, column = 0)
         
@@ -44,8 +46,9 @@ class Parqueadero:
         boton_piso2 = ctk.CTkButton(master=self.principal,
                                     text= "Piso 2",
                                     command= self.botonPiso2,
-                                    height= 20,
+                                    height= 25,
                                     width= 200,
+                                    corner_radius= 15
                                     )
         boton_piso2.grid(row = 0, column = 1)
         
@@ -53,22 +56,70 @@ class Parqueadero:
         boton_piso3 = ctk.CTkButton(master=self.principal,
                                     text= "Piso 3",
                                     command= self.botonPiso3,
-                                    height= 20,
+                                    height= 25,
                                     width= 200,
+                                    corner_radius=15
                                     )
         boton_piso3.grid(row = 0, column = 2)
         
         #Panel que se usará para centrar los botones de disponibilidad de los pisos
-        panel = ctk.CTkFrame(master=self.principal,
+        panel1 = ctk.CTkFrame(master=self.principal,
                              height= 560,
                              width= 600,
                              corner_radius= 15,
                              )
-        panel.place(relx=0.345, rely=0.515, anchor=ctk.CENTER)
+        panel1.place(relx=0.345, rely=0.515, anchor=ctk.CENTER)
+        
+        #Panel que se usará para centrar el ingreso/informacion de vehiculos
+        panel2 = ctk.CTkFrame(master=self.principal,
+                              height=560,
+                              width=200,
+                              corner_radius=15
+                              )
+        panel2.place(relx = 0.850, rely = 0.515, anchor = ctk.CENTER)
         
         
+        #Titulo "Informacion de vehiculo"
+        informacion_vehiculo_titulo = ctk.CTkLabel(master=panel2,
+                                            text= "Informacion del vehiculo"
+                                            )
+        informacion_vehiculo_titulo.place(relx = 0.5, rely = 0.04, anchor = ctk.CENTER)
         
+        #Texto que mostrará la informacion del vehiculo
+        informacion_vehiculo = ctk.CTkLabel(master= panel2,
+                                            text= "Sin informacion"
+                                            )
+        informacion_vehiculo.place(relx = 0.5, rely = 0.1, anchor = ctk.CENTER)
+               
+        #TextField de placa para la busqueda de un vehiculo
+        self.buscar_placa = ctk.CTkEntry(master= panel2,
+                                    height= 25,
+                                    width= 180,
+                                    corner_radius= 15,
+                                    placeholder_text= "          Placa del vehiculo",
+                                    placeholder_text_color= "gray"                          
+                                    )
+        self.buscar_placa.place(relx = 0.5, rely = 0.160, anchor = ctk.CENTER)
         
+        #Boton para buscar vehiculo mediante la placa
+        buscar_vehiculo = ctk.CTkButton(master= panel2,
+                                        height=25,
+                                        width=180,
+                                        text="Buscar vehiculo",
+                                        corner_radius= 15,
+                                        command=self.buscarVehiculo
+                                        )
+        buscar_vehiculo.place(relx = 0.5, rely = 0.220, anchor = ctk.CENTER)
+        
+        #Boton eliminar carro
+        eliminar_carro = ctk.CTkButton(master=panel2,
+                                       text= "Eliminar vehiculo",
+                                       command=self.eliminarVehiculo,
+                                       height= 25,
+                                       width= 180,
+                                       corner_radius= 15
+                                       )
+        eliminar_carro.place(relx = 0.5, rely = 0.280 , anchor = ctk.CENTER)
         
         self.principal.mainloop()
     
@@ -90,6 +141,37 @@ class Parqueadero:
     def botonPiso3(self):
         print("Boton Piso 3")
         
+    def buscarVehiculo(self):
+        placa = self.buscar_placa.get()
+        if placa == "":
+            print("Digite una placa valida con el formato XXX000")
+        print(placa)
+        
+    def eliminarVehiculo(self):
+        print("Eliminar vehiculo")
+    
+    #Para verificar si la placa es valida
+    def verificarPlaca(self):
+        placa = self.buscar_placa.get()
+        if self.formatoPlacaCorrecto(placa):
+            print("Formato de placa válido")
+            return True
+        else:
+            print("Formato de placa inválido")
+            print
+            return False
+
+    def formatoPlacaCorrecto(self, placa):
+        # Expresión regular para verificar el formato de placa colombiano
+        patron = r'^[A-Z]{3}\d{3}$'
+        return re.match(patron, placa) is not None
+    
+        #Expresión Regular: ^[A-Z]{3}\d{3}$
+        #^ indica el inicio de la línea.
+        #[A-Z]{3} verifica que hay tres letras mayúsculas.
+        #\d{3} verifica que hay tres dígitos.
+        #$ indica el final de la línea.
+        #re.match(patron, placa): Comprueba si la cadena placa coincide con el patrón. Retorna un objeto de coincidencia si la placa es válida, y None si no lo es.
         
         
         
